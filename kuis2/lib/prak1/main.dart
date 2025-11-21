cb04cf2a9933fa48debd38c76208e9204061adcd
PuteraBhagas <p.bhagaswara@gmail.com>
2025-11-21 09:13:53 +0700

    prak 1

---
+++ kuis2/lib/prak1/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:kuis2/User.dart';
+
+void main() {
+  User user = User(
+    id: 1, 
+    name: 'John Doe', 
+    email: 'john@example.com', 
+    createdAt: DateTime.now());
+
+    Map<String, dynamic> userJson = user.toJson();
+    print('User JSON: $userJson');
+
+    Map<String, dynamic> jsonData ={
+      'id': 2,
+      'name': 'Jane Doe',
+      'email': 'jane@example.com',
+      'created_at': '2024-01-01T10:00:00.000Z',
+    };
+
+    User userFromJson = User.fromJson(jsonData);
+    print('User from JSON: ${userFromJson.name}');
+}
